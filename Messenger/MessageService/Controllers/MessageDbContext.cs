573d6d39b84c14f56a3df74c00cc44c96d7ac436
n.dumtsev <n.dumtsev@gmail.com>
2025-02-11 00:45:47 +0300

    Initial commit

---
+++ Messenger/MessageService/Controllers/MessageDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MessageService.Controllers
+{
+	public class MessageDbContext : DbContext
+	{
+		public DbSet<Message> Messages { get; set; }
+		public MessageDbContext(DbContextOptions<MessageDbContext> options) : base(options)
+		{
+		}
+	}
+}
---
+++ Messenger/MessageService/Controllers/MessagesController.cs
@@ -0,0 +1,36 @@
+﻿using MessageService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MessagesController : ControllerBase
+{
+	private readonly MessageDbContext _db;
+
+	public MessagesController(MessageDbContext db)
+	{
+		_db = db;
+	}
+
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<Message>>> GetMessages()
+	{
+		return await _db.Messages.ToListAsync();
+	}
+
+	[HttpPost]
+	public async Task<ActionResult<Message>> SendMessage([FromBody] string text)
+	{
+		Console.WriteLine("Работа метода SendMessage");
+		var message = new Message { 
+			text = text,
+			dateTime = DateTime.UtcNow
+		};
+
+		_db.Messages.Add(message);
+		await _db.SaveChangesAsync();
+		return Ok(message);
+	}
+}
+
