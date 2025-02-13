0c1eb94195c8234b135a01035a44e5ccccbfa456
n.dumtsev <n.dumtsev@gmail.com>
2025-02-14 01:17:54 +0300

    6

---
+++ Messenger/Pingo.Messages.WebApi/Controllers/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace MessageService.Controllers
+{
+	public class Message
+	{
+		public int id { get; set; }
+		public string text { get; set; }
+		public DateTime dateTime { get; set; }
+	}
+}
