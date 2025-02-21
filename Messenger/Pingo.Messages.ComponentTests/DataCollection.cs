a85b540bbadae1a7ec2343b5991ab1bc7154d8e1
Nilokay <87380184+k0milfo@users.noreply.github.com>
2025-02-21 04:30:10 +0300

    Delete Messenger directory

--- Messenger/Pingo.Messages.ComponentTests/DataCollection.cs
+++
@@ -1,6 +0,0 @@
-namespace Pingo.Messages.ComponentTests;
-
-    [CollectionDefinition("Database Collection", DisableParallelization = true)]
-    public sealed class DataCollection : ICollectionFixture<WebAppFactoryFixture>
-    {
-    }
--- Messenger/Pingo.Messages/Entity/Message.cs
+++
@@ -1,3 +0,0 @@
-namespace Pingo.Messages.Entity;
-
-public sealed record Message(Guid Id, string Text, DateTimeOffset SentAt, DateTimeOffset? UpdatedAt);
